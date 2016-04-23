0b5ec517c71c8a83d55b587e26e23554350061b2
mitchconquer <mitch@robotshanghai.com>
2016-04-23 15:18:18 -0700

    Correct User.username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
 end
